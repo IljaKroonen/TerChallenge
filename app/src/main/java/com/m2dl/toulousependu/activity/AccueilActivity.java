5514d3826f79498186d466de467221396eebb1c3
IljaKroonen <ilja.kroonen@gmail.com>
2015-01-22 14:06:04 +0100

    Attempt at fixing bug - but it remains unchallenged to this day

--- app/src/main/java/com/m2dl/toulousependu/activity/AccueilActivity.java
+++ app/src/main/java/com/m2dl/toulousependu/activity/AccueilActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.m2dl.toulousependu.R;
 
@@ -16,15 +17,19 @@ public class AccueilActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        Button scoresButton = (Button) findViewById(R.id.scoresButton);
+        scoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccueilActivity.this, ScoresActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void newGame(View v) {
         Intent intent = new Intent(this,DifficultyActivity.class);
-        startActivity(intent);
-    }
-
-    public void displayScores(View v) {
-        Intent intent = new Intent(this, ScoresActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/m2dl/toulousependu/activity/ScoresActivity.java
+++ app/src/main/java/com/m2dl/toulousependu/activity/ScoresActivity.java
@@ -1,9 +1,12 @@
 package com.m2dl.toulousependu.activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.m2dl.toulousependu.R;
 
@@ -13,28 +16,13 @@ public class ScoresActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scores, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ScoresActivity.this.navigateUpTo(new Intent(ScoresActivity.this, AccueilActivity.class));
+            }
+        });
     }
 }
