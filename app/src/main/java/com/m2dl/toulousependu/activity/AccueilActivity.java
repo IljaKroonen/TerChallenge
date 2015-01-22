f96c22c0913c0d323bae70a8d2e94ac8549ec53c
SimkaDev <sion.romain@live.fr>
2015-01-22 09:47:14 +0100

    Difficulty activity

--- app/src/main/java/com/m2dl/toulousependu/activity/AccueilActivity.java
+++ app/src/main/java/com/m2dl/toulousependu/activity/AccueilActivity.java
@@ -1,7 +1,9 @@
 package com.m2dl.toulousependu.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.m2dl.toulousependu.R;
 
@@ -14,6 +16,11 @@ public class AccueilActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+    }
+
+    public void newGame(View v) {
+        Intent intent = new Intent(this,DifficultyActivity.class);
+        startActivity(intent);
     }
 
 
