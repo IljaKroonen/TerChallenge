3c151313796cad8bc0a1795ad48bfa1d19c1c25b
SimkaDev <sion.romain@live.fr>
2015-01-22 10:47:01 +0100

    Pendu draw

---
+++ app/src/main/java/com/m2dl/toulousependu/utils/DrawingView.java
@@ -0,0 +1,82 @@
+package com.m2dl.toulousependu.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Romain on 22/01/2015.
+ */
+public class DrawingView extends View implements View.OnTouchListener {
+
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xFF660000;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
+    public DrawingView(Context context) {
+        super(context);
+        initPaint();
+    }
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        initPaint();
+    }
+
+
+    private void initPaint() {
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        this.invalidate();
+        return true;
+    }
+
+}
