6da8957921bc7ffb619d2d4459aec68e5ac201cf
Lifegnard <math.bertoia@gmail.com>
2015-01-22 11:43:00 +0100

    get lettre pour un mot

--- app/src/main/java/com/m2dl/toulousependu/activity/PenduActivity.java
+++ app/src/main/java/com/m2dl/toulousependu/activity/PenduActivity.java
@@ -7,13 +7,16 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.m2dl.toulousependu.R;
 import com.m2dl.toulousependu.utils.DrawingView;
+import com.m2dl.toulousependu.var.GlobalVars;
 
 import org.w3c.dom.Text;
 
@@ -26,6 +29,7 @@ public class PenduActivity extends Activity{
     private DrawingView pendu;
     private int difficulty;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +37,36 @@ public class PenduActivity extends Activity{
         difficulty = getIntent().getIntExtra(DifficultyActivity.TAG_DIFFICULTE,1);
         pendu = (DrawingView) findViewById(R.id.pendu);
         pendu.setOnTouchListener(pendu);
+        affichageLettre();
     }
 
     public void letter(View v) {
         Button button = (Button) v;
         Log.d("LETTER",button.getText()+"");
+    }
+
+
+    public String recupGlobalvar(){
+       return GlobalVars.csvWordProvider.getWord(difficulty);
+    }
+
+    public void affichageLettre(){
+        String mot = recupGlobalvar();
+        char lettre;
+
+
+        for (int i = 0 ; i < mot.length() ;i++)
+           lettre=mot.charAt(i);
+            TextView textView = new TextView(this);
+            textView.setBackgroundResource(R.drawable.letter_pendu);
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(60);
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            llp.setMargins(5, 5, 5, 5); // llp.setMargins(left, top, right, bottom);
+            textView.setLayoutParams(llp);
+            LinearLayout result = (LinearLayout) findViewById(R.id.layout_result);
+            result.addView(textView);
+
     }
 
 
