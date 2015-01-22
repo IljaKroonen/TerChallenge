1f8dd79f9f21050611b25923442d9c698aa2f984
IljaKroonen <ilja.kroonen@gmail.com>
2015-01-22 10:50:23 +0100

    Wrote game algorithms

--- app/src/main/java/com/m2dl/toulousependu/game/Game.java
+++ app/src/main/java/com/m2dl/toulousependu/game/Game.java
@@ -1,5 +1,6 @@
 package com.m2dl.toulousependu.game;
 
+import java.text.Normalizer;
 import java.util.Locale;
 
 /**
@@ -7,12 +8,19 @@ import java.util.Locale;
  * Created by Ilja on 22/01/2015.
  */
 public class Game {
+    private static final int LIVES = 6;
+
     private char[] word;
+    private char[] wordWithoutAccents;
     private char[] found;
+    private int remainingLives;
 
     public Game(String word) {
         String upper = word.toUpperCase(Locale.FRANCE);
         this.word = upper.toCharArray();
+        word = Normalizer.normalize(word, Normalizer.Form.NFD);
+        word = word.replaceAll("[^\\p{ASCII}]", "");
+        wordWithoutAccents = word.toCharArray();
         found = new char[this.word.length];
     }
 
@@ -24,7 +32,44 @@ public class Game {
         return 0;
     }
 
+    public boolean isFinished() {
+        if (remainingLives >= LIVES) {
+            return true;
+        }
+
+        for (int i = 0; i < found.length; i++) {
+            if (found[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isVictory() {
+        for (int i = 0; i < found.length; i++) {
+            if (found[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public boolean inputLetter(char l) {
-        return false;
+        boolean present = false;
+
+        for (int i = 0; i < wordWithoutAccents.length; i++) {
+            if (l == wordWithoutAccents[i]) {
+                found[i] = word[i];
+                present = true;
+            }
+        }
+
+        if (!present) {
+            remainingLives++;
+        }
+
+        return present;
     }
 }
