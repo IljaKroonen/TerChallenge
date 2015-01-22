4dc5b016d45b5043281ee1d181c006cf5970118c
IljaKroonen <ilja.kroonen@gmail.com>
2015-01-22 09:48:44 +0100

    Work on word provider

--- app/src/main/java/com/m2dl/toulousependu/resourcehelpers/IWordProvider.java
+++ app/src/main/java/com/m2dl/toulousependu/resourcehelpers/IWordProvider.java
@@ -4,5 +4,5 @@ package com.m2dl.toulousependu.resourcehelpers;
  * Created by Ilja on 22/01/2015.
  */
 public interface IWordProvider {
-    String getWord(Difficulty difficulty);
+    String getWord(int difficulty);
 }
